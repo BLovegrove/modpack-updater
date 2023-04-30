522f6d1352fd7e1d62e4023b22d1f5073e6e9e63
Brandon L <brandon@lovegrove.co.nz>
2023-04-30 13:37:17 +1200

    streamlined process queue, fixed comments, and added dry run function

--- update/__main__.py
+++ update/__main__.py
@@ -11,9 +11,16 @@ import config as cfg
 
 
 def main():
+    # check for dry run arg -------------------------------------------------------------------------- #
+    if "-d" in sys.argv or "--dryrun" in sys.argv:
+        dry_run = True
+        print("! == THIS IS A DRY RUN == !")
+    else:
+        dry_run = False
+
     os.chdir(os.path.dirname(__file__))
 
-    # Welcome message
+    # welcome message -------------------------------------------------------------------------------- #
     print(
         f"# ---------------------------------------------- #{os.linesep}"
         + f"#                 Modpack Update                 #{os.linesep}"
@@ -22,7 +29,7 @@ def main():
         + f"Thank you for choosing {cfg.pack.name}!{os.linesep*2}"
     )
 
-    # check client can see server
+    # check client can see server -------------------------------------------------------------------- #
     split_url = urlsplit(cfg.pack.url)
     try:
         response = int(
@@ -51,8 +58,7 @@ def main():
     else:
         print("Connection status: ✅" + os.linesep)
 
-    # check current version against most recent remote version
-
+    # check current version against most recent remote version --------------------------------------- #
     changelog = toml.loads(
         requests.get(cfg.pack.url + "/changelog.toml", stream=True).content.decode()
     )
@@ -64,7 +70,7 @@ def main():
         pathlib.Path("../version.txt").write_text("0.0.0", encoding="utf-8")
 
     current_version = pathlib.Path("../version.txt").read_text()
-    update_queue: list[dict[str, str]] = []
+    update_queue: list[dict[str, any]] = []
 
     for update in changelog["updates"]:
         if version.parse(update["version"]) > version.parse(current_version):
@@ -79,69 +85,61 @@ def main():
 
     print("Update detected! Collating changes since last update..." + os.linesep)
 
-    # parse changes into add and remove lists
-
-    process_queue: list[dict[str, str]] = []
-    rem_queue = []
-    add_queue = []
+    # process updates into single process queue ------------------------------------------------------ #
+    process_queue: dict[str, list[str]] = {"download": [], "remove": []}
 
     for update in update_queue:
         for key in update.keys():
-            if key == "version":
+            if key == "version" or key == "timestamp":
                 continue
 
-            try:
-                for item in update[key]["rem"]:
-                    if item not in rem_queue:
-                        rem_queue.append(item)
-                        process_queue.append({"action": "rem", "file": item})
-            except KeyError as e:
-                pass
+            print(f"update: {update.items()}, key: {key}")
 
             try:
                 for item in update[key]["add"]:
-                    if item not in add_queue:
-                        add_queue.append(item)
-                        process_queue.append({"action": "add", "file": item})
+                    if item not in process_queue["download"]:
+                        process_queue["download"].append(item)
             except KeyError as e:
                 pass
 
-    # execute process queue downloading and deleting files where needed
-
-    for item in process_queue:
-        queue_display = f"(item #{process_queue.index(item)+1} of {len(process_queue)})"
-
-        file = item["file"]
-
-        if file.split(".")[-1] == "jar":
-            item_type = "mods"
-        else:
-            item_type = "config"
-
-        filepath = f"../{item_type}/{file}"
-
-        if item["action"] == "rem":
             try:
-                print(f"Removing '{file}' {queue_display}")
-                os.remove(filepath)
-            except FileNotFoundError as e:
-                print(f"File not found. Can't remove.")
-        else:
-            print(f"Creating '{file}' {queue_display}")
-            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+                for item in update[key]["rem"]:
+                    if item not in process_queue["remove"]:
+                        process_queue["remove"].append(item)
+            except KeyError as e:
+                pass
 
-            if os.path.exists(filepath):
-                os.remove(filepath)
+    # execute process queue downloading and deleting files where needed ------------------------------ #
+    print("Executing changes..." + os.linesep)
+    for action, files in process_queue.items():
+        for file in files:
+            queue_display = f"(item #{files.index(file)+1} of {len(files)})"
 
-            with open(filepath, "wb") as f:
-                f.write(requests.get(f"{cfg.pack.url}/{item_type}/{file}").content)
+            filepath = f"../{file}"
 
-    # change local version to match remote
+            if action == "remove":
+                try:
+                    print(f"Removing '{file}' {queue_display}")
+                    if not dry_run:
+                        os.remove(filepath)
+                except FileNotFoundError as e:
+                    print(f"File not found. Can't remove.")
+            else:
+                print(f"Creating '{file}' {queue_display}")
+                if not dry_run:
+                    os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
+                    if os.path.exists(filepath):
+                        os.remove(filepath)
+
+                    with open(filepath, "wb") as f:
+                        f.write(requests.get(f"{cfg.pack.url}/{file}").content)
+
+    # change local version to match remote ----------------------------------------------------------- #
     with open("../version.txt", "w") as f:
         f.write(update_queue[0]["version"])
 
-    # finish up and close
+    # finish up and close ---------------------------------------------------------------------------- #
     messagebox.showinfo(
         title="Success!",
         message=f"Modpack updated.{os.linesep*2}Launching your instance now...",
