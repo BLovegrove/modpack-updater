7441d7ea8e1ada28256de75c9d9536b0d5a5c7fa
Brandon <brandon@lovegrove.co.nz>
2023-05-02 01:32:16 +1200

    fixed config_example to show chnagelog.toml instead of .txt

--- config_example.py
+++ config_example.py
@@ -1,7 +1,7 @@
 class pack:
     # The name of your pack to display in the scripts welcome message
     name = "My Modpack"
-    # Url to your modpacks data. Must be a remote url containing a mods folder and config folder in its root, as well as your changelog.txt file.
+    # Url to your modpacks data. Must be a remote url containing a mods folder and config folder in its root, as well as your changelog.toml file.
     url = "https://foo.bar/modpack/"
 
 
